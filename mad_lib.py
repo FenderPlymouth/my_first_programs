79cc64d0d7f0066be76a2d188d54f437ff3f88e5
Josh Fender <52008831+FenderPlymouth@users.noreply.github.com>
2020-12-14 14:24:31 -0500

    completed the mad lib

--- mad_lib.py
+++ mad_lib.py
@@ -5,7 +5,7 @@
 
 # Ask for user inputs for all of the blank words.
 color1 = input("Give me a color: ")
-verb = input("Give me a past tense verb: ")
+verb1 = input("Give me a past tense verb: ")
 number = input("Give me a number: ")
 animal = input("Give me an animal: ")
 adj = input("Give me an adjective: ")
@@ -13,19 +13,20 @@ tool = input("Give me a tool: ")
 veg = input("Give me a vegetable: ")
 cont = input("Give me a container: ")
 color2 = input("Give me another color: ")
-'''noun1 = input("Give me a noun: ")
+noun1 = input("Give me a noun: ")
 fruit = input("Give me a fruit: ")
 candy = input("Give me type of candy: ")
 noun2 = input("Give me another noun: ")
 noun3 = input("Give me a third noun: ")
+verb2 = input("Give me another verb: ")
 furn = input("Give me a peice of furnature: ")
 color3 = input("Give me a third color: ")
 p_noun1 = input("Give me a plural noun: ")
-p_noun2 = input("Give me another plural noun: ")'''
+p_noun2 = input("Give me another plural noun: ")
 
 # Display the mad lib with each sentence getting a new print command
 print("Today we had a substitue teacher for science class, with " + color1 +
-      " hair that " + verb +
+      " hair that " + verb1 +
       " straight up " + number +
       " inches high.")
 print("His name was Mr. " + animal +
@@ -34,3 +35,15 @@ print("First, he used a " + tool +
       " and a " + veg +
       " to make a " + cont +
       " of water turn " + color2 + ".")
+print("Then he made a " + noun1 +
+      " of the solar system using " + fruit +
+      ", " + candy +
+      ", and " + noun2 + ".")
+print("When the principle walked by and saw Mr. " + animal +
+      " using " + noun3 +
+      " to " + verb2 +
+      " the " + furn +
+      " into " + color3 +
+      " " + p_noun1 +
+      ", she asked him to she the class a movie about" + p_noun2 + " instead.")
+print("The next day, we had a differnt substitute teacher.")
